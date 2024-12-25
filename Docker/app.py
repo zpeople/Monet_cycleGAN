0d367d40f3ecb1f9edcd469f17e9acb2f1b4657a
zpeople <6829549@qq.com>
2024-12-25 18:12:30 +0800

    update docker

--- Docker/app.py
+++ Docker/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, jsonify
-import joblib
+
 import numpy as np
 
 app = Flask(__name__)
 
-# 加载训练好的模型
-model = joblib.load('model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    prediction = model.predict([data['features']])
+    prediction = 'test'
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
